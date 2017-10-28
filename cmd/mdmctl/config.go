65dc6b4417897352905faeae094b46cfe677fcb6
Nate Walck <nate.walck@gmail.com>
2017-10-27 20:23:38 -0400

    Added ability to store multiple servers and switch between them

--- cmd/mdmctl/config.go
+++ cmd/mdmctl/config.go
@@ -13,6 +13,7 @@ import (
 	"strings"
 
 	"crypto/tls"
+	"github.com/pkg/errors"
 	"net/http"
 )
 
@@ -37,16 +38,12 @@ func (cmd *configCommand) Run(args []string) error {
 		os.Exit(1)
 	}
 
-	var config *ClientConfig
-	if cfg, err := LoadClientConfig(); err == nil {
-		config = cfg
-	} else {
-		config = new(ClientConfig)
-	}
-	var run func(*ClientConfig, []string) error
+	var run func([]string) error
 	switch strings.ToLower(args[0]) {
 	case "set":
 		run = setCmd
+	case "switch":
+		run = switchCmd
 	case "print":
 		printConfig()
 		return nil
@@ -55,7 +52,7 @@ func (cmd *configCommand) Run(args []string) error {
 		os.Exit(1)
 	}
 
-	return run(config, args[1:])
+	return run(args[1:])
 }
 
 func printConfig() {
@@ -79,9 +76,10 @@ mdmctl config set -h
 	return nil
 }
 
-func setCmd(cfg *ClientConfig, args []string) error {
+func setCmd(args []string) error {
 	flagset := flag.NewFlagSet("set", flag.ExitOnError)
 	var (
+		flName       = flagset.String("name", "", "name of the server")
 		flToken      = flagset.String("api-token", "", "api token to connect to micromdm server")
 		flServerURL  = flagset.String("server-url", "", "server url of micromdm server")
 		flSkipVerify = flagset.Bool("skip-verify", false, "skip verification of server certificate (insecure)")
@@ -91,6 +89,8 @@ func setCmd(cfg *ClientConfig, args []string) error {
 	if err := flagset.Parse(args); err != nil {
 		return err
 	}
+
+	cfg := new(ServerConfig)
 
 	if *flToken != "" {
 		cfg.APIToken = *flToken
@@ -104,7 +104,21 @@ func setCmd(cfg *ClientConfig, args []string) error {
 
 	cfg.SkipVerify = *flSkipVerify
 
-	return SaveClientConfig(cfg)
+	return saveServerConfig(cfg, *flName)
+}
+
+func switchCmd(args []string) error {
+	flagset := flag.NewFlagSet("set", flag.ExitOnError)
+	var (
+		flName = flagset.String("name", "", "name of the server to switch to")
+	)
+
+	flagset.Usage = usageFor(flagset, "mdmctl config switch [flags]")
+	if err := flagset.Parse(args); err != nil {
+		return err
+	}
+
+	return SwitchServerConfig(*flName)
 }
 
 func validateServerURL(serverURL string) (string, error) {
@@ -129,10 +143,10 @@ func clientConfigPath() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return filepath.Join(usr.HomeDir, ".micromdm", "default.json"), err
+	return filepath.Join(usr.HomeDir, ".micromdm", "servers.json"), err
 }
 
-func SaveClientConfig(cfg *ClientConfig) error {
+func SaveClientConfig(clientCfg *ClientConfig) error {
 	configPath, err := clientConfigPath()
 	if err != nil {
 		return err
@@ -147,13 +161,36 @@ func SaveClientConfig(cfg *ClientConfig) error {
 		return err
 	}
 	defer f.Close()
-
-	if cfg == nil {
-		cfg = new(ClientConfig)
-	}
 	enc := json.NewEncoder(f)
 	enc.SetIndent("", "  ")
-	return enc.Encode(cfg)
+	return enc.Encode(clientCfg)
+}
+
+func saveServerConfig(cfg *ServerConfig, name string) error {
+	clientCfg, err := LoadClientConfig()
+	if err != nil {
+		if os.IsNotExist(errors.Cause(err)) {
+			clientCfg = new(ClientConfig)
+			clientCfg.Servers = make(map[string]ServerConfig)
+		} else {
+			return err
+		}
+	}
+	fmt.Println(clientCfg)
+	if cfg == nil {
+		cfg = new(ServerConfig)
+	}
+	clientCfg.Servers[name] = *cfg
+	return SaveClientConfig(clientCfg)
+}
+
+func SwitchServerConfig(name string) error {
+	clientCfg, err := LoadClientConfig()
+	if err != nil {
+		return err
+	}
+	clientCfg.Active = name
+	return SaveClientConfig(clientCfg)
 }
 
 func LoadClientConfig() (*ClientConfig, error) {
@@ -163,7 +200,7 @@ func LoadClientConfig() (*ClientConfig, error) {
 	}
 	cfgData, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("unable to load default config file: %s", err)
+		return nil, errors.Wrap(err, "unable to load default config file")
 	}
 	var cfg ClientConfig
 	err = json.Unmarshal(cfgData, &cfg)
@@ -173,7 +210,22 @@ func LoadClientConfig() (*ClientConfig, error) {
 	return &cfg, nil
 }
 
+func LoadServerConfig() (*ServerConfig, error) {
+	cfg, err := LoadClientConfig()
+	if err != nil {
+		return nil, err
+	}
+	var serverCfg ServerConfig
+	serverCfg = cfg.Servers[cfg.Active]
+	return &serverCfg, nil
+}
+
 type ClientConfig struct {
+	Active  string                  `json:"active"`
+	Servers map[string]ServerConfig `json:"servers"`
+}
+
+type ServerConfig struct {
 	APIToken   string `json:"api_token"`
 	ServerURL  string `json:"server_url"`
 	SkipVerify bool   `json:"skip_verify"`
